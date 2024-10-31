9b95c6a0c5181070856653c0adf663a5cccf0efa
DaniloDuque <dduque@estudiantec.cr>
2024-10-31 00:47:28 -0600

    ...

--- code/FileIndexServer/util.h
+++ code/FileIndexServer/util.h
@@ -1,4 +1,12 @@
 #ifndef UTIL_HEADER
 #define UTIL_HEADER
 
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
 #endif
