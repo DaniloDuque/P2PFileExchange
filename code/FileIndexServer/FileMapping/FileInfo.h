0e480a17c64a67d45b7805223709af3e4bd2448c
DaniloDuque <dduque@estudiantec.cr>
2024-11-01 23:33:19 -0600

    ...

--- code/FileIndexServer/FileMapping/FileInfo.h
+++ code/FileIndexServer/FileMapping/FileInfo.h
@@ -1,11 +1,18 @@
 #include <iostream>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/trie_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename Key, typename Value = null_type>
+using trie = trie<Key, Value, trie_string_access_traits<>, pat_trie_tag, trie_prefix_search_node_update>;
 
 template<typename T>
-class FileMapper {
+class FileInfo {
 
 private:
     T hash1, hash2, size;
-    vector<string> aka;
+    trie<string> aka;
 
 public:
 
