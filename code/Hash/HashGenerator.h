f3036c9c663645ed6f3a95adb60ad80f3c93c2a0
DaniloDuque <dduque@estudiantec.cr>
2024-11-04 00:15:57 -0600

    ...

--- code/Hash/HashGenerator.h
+++ code/Hash/HashGenerator.h
@@ -1,13 +1,7 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <utility>
+#include "../util.h"
 
-const size_t BUFFER_SIZE = 4096; 
-
-
-long long polynomialHash(const char* data, size_t length, long long base = 257, long long mod = 9223372036854775783LL) {
-    long long hashValue = 0;
+ll polynomialHash(const char* data, size_t length, ll base = 257, ll mod = 9223372036854775783LL) {
+    ll hashValue = 0;
     for (size_t i = 0; i < length; ++i) {
         hashValue = (hashValue * base + data[i]) % mod;
     }
@@ -15,11 +9,11 @@ long long polynomialHash(const char* data, size_t length, long long base = 257, 
 }
 
 
-long long fnv1aHash(const char* data, size_t length, long long mod = 9223372036854775783LL) {
-    const long long FNV_PRIME = 1099511628211LL;
-    const long long FNV_OFFSET_BASIS = 14695981039346656037ULL;
+ll fnv1aHash(const char* data, size_t length, ll mod = 9223372036854775783LL) {
+    const ll FNV_PRIME = 1099511628211LL;
+    const ll FNV_OFFSET_BASIS = 14695981039346656037ULL;
     
-    long long hashValue = FNV_OFFSET_BASIS;
+    ll hashValue = FNV_OFFSET_BASIS;
     for (size_t i = 0; i < length; ++i) {
         hashValue ^= data[i];
         hashValue = (hashValue * FNV_PRIME) % mod;  
@@ -28,7 +22,7 @@ long long fnv1aHash(const char* data, size_t length, long long mod = 92233720368
 }
 
 
-std::pair<long long int, long long int> doubleHashFile(const std::string& filePath) {
+std::pair<ll, ll> doubleHashFile(const std::string& filePath) {
     std::ifstream file(filePath, std::ios::binary);
     if (!file.is_open()) {
         std::cerr << "Could not open the file!" << std::endl;
@@ -36,18 +30,13 @@ std::pair<long long int, long long int> doubleHashFile(const std::string& filePa
     }
 
     char buffer[BUFFER_SIZE];
-    long long hash1 = 0;
-    long long hash2 = 0;
-    const long long mod = 9223372036854775783LL;
-
+    ll hash1 = 0;
+    ll hash2 = 0;
+    const ll mod = 9223372036854775783LL;
     
     while (file.read(buffer, BUFFER_SIZE) || file.gcount() > 0) {
         size_t bytesRead = file.gcount();
-
-        
         hash1 = (hash1 + polynomialHash(buffer, bytesRead)) % mod;
-
-        
         hash2 = (hash2 + fnv1aHash(buffer, bytesRead)) % mod;
     }
 
