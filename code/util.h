18763afe6342c15b9f754ccfa652333e3cad146f
DaniloDuque <dduque@estudiantec.cr>
2024-11-04 00:27:54 -0600

    ...

--- code/util.h
+++ code/util.h
@@ -7,8 +7,14 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <thread>
+#include <utility>
+#include <fstream>
 
+#define INDEX_PORT 8080
+#define INDEX_IP "6.tcp.ngrok.k"
+#define PEER_PORT 8080
 #define BUFFER_SIZE (1<<13)
 #define byte unsigned char
+#define ll long long
 
 #endif
