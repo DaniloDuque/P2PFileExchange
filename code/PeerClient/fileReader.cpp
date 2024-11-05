8d54955fb1926ca6da00cbac2d493ee218ed9b82
kyaki <emanuelrs101@gmail.com>
2024-11-04 21:30:52 -0600

    Fixed IP

--- code/PeerClient/fileReader.cpp
+++ code/PeerClient/fileReader.cpp
@@ -15,11 +15,14 @@ std::vector<PeerFileDTO<long long>>  fileDirectoryReader(std::string & directory
         std::string name = dir_entry.path().filename();
         PeerFileDTO<long long> DTO;
         DTO.h1 = hash.first;
-        DTO.h1 = hash.second;
+        DTO.h2 = hash.second;
         DTO.size = std::filesystem::file_size(dir_entry);
         DTO.alias = name;
         PeerInfo info;
         info.ip.sin6_family = AF_INET6;
+
+        std::string ip_str = "2001:0db8:85a3:0000:0000:8a2e:0370:7334";
+        inet_pton(AF_INET6, ip_str.c_str(), &info.ip.sin6_addr);
         info.ip.sin6_port = htons(8080);
         info.port = 8080;
         DTO.info = info;
