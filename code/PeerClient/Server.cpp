356bfd17cd0e210ed4b83890ae5a6ff80b5381c4
kyaki <emanuelrs101@gmail.com>
2024-11-04 22:54:32 -0600

    Added net to peer server

--- code/PeerClient/Server.cpp
+++ code/PeerClient/Server.cpp
@@ -4,11 +4,28 @@
 
 
 
-int main() {
+int main(int argc, char const * argv[]) {
+
+    
+    std::string mainPort = argv[1];
+    std::string indexIp = argv[2];
+    std::string indexPort = argv[3];
 
     vector<PeerFileDTO<long long>> list;
 
     std::string directory = "FileReader/testDirectory";
+
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    struct hostent *server = gethostbyname(indexIp.c_str());
+    memcpy(&serverAddress.sin_addr.s_addr, server->h_addr_list[0], server->h_length);
+
+    connect(clientSocket, (struct sockaddr*) &serverAddress, sizeof(serverAddress));
+
 
     list = fileDirectoryReader(directory);
     std::string package = "1 ";
@@ -19,6 +36,9 @@ int main() {
         }
 
     }
+
+    send(clientSocket, package.c_str(), package.size(), 0);
+
     std::cout << package << endl;
 
 
