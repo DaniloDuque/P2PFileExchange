a87bbd28ddd55b12303dbfabcfd8a598eba82cc8
kyaki <emanuelrs101@gmail.com>
2024-11-04 21:39:05 -0600

    Created package to send to main server from client, needs testing...

--- code/PeerClient/Server.cpp
+++ code/PeerClient/Server.cpp
@@ -11,11 +11,15 @@ int main() {
     std::string directory = "FileReader/testDirectory";
 
     list = fileDirectoryReader(directory);
-    for(auto i : list) {
-
-        std::cout << i.serialize() << std::endl;
+    std::string package = "1 ";
+    for(int i = 0; i < list.size(); i++) {
+        package += list[i].serialize();
+        if(i != list.size() - 1) {
+            package += ' ';
+        }
 
     }
+    std::cout << package << endl;
 
 
 
--- code/PeerClient/fileReader.cpp
+++ code/PeerClient/fileReader.cpp
@@ -11,7 +11,6 @@ std::vector<PeerFileDTO<long long>>  fileDirectoryReader(std::string & directory
     for(auto const & dir_entry : std::filesystem::directory_iterator(sandbox)) {
         
         std::pair<long long int, long long int> hash = doubleHashFile(dir_entry.path());
-        std::cout << hash.first << ' ' << hash.second <<' ' << std::filesystem::file_size(dir_entry) << std::endl;
         std::string name = dir_entry.path().filename();
         PeerFileDTO<long long> DTO;
         DTO.h1 = hash.first;
