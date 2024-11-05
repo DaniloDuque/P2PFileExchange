58c61fb18e780efce5a6ba16a1675ee76295b687
DaniloDuque <dduque@estudiantec.cr>
2024-11-04 19:01:40 -0600

    ...

--- code/util.h
+++ code/util.h
@@ -4,12 +4,14 @@
 #include <iostream>
 #include <thread>
 #include <string>
+#include <sstream>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <thread>
 #include <utility>
 #include <fstream>
+#include <vector>
 
 #define INDEX_PORT 8080
 #define INDEX_IP "6.tcp.ngrok.k"
@@ -19,5 +21,13 @@
 #define uchar unsigned char
 
 using namespace std;
+
+string sockaddr_in6_to_string(const sockaddr_in6& addr) {
+    char str[INET6_ADDRSTRLEN]; 
+    if (inet_ntop(AF_INET6, &addr.sin6_addr, str, sizeof(str)) == nullptr) {
+        return "Error en conversión"; 
+    }
+    return string(str); 
+}
 
 #endif
